a00928ba9ef8143918fa022818d6ad69d5c80ec0
Nikolay Mikheev <me@vooft.com>
2014-09-03 16:31:31 +0400

    [+] Test for SimplePieceFactory class

---
+++ src/test/java/com/vooft/trycatch/board/SimplePieceFactoryTest.java
@@ -0,0 +1,28 @@
+package com.vooft.trycatch.board;
+
+import com.vooft.trycatch.common.BoardSize;
+import com.vooft.trycatch.pieces.PieceType;
+import com.vooft.trycatch.pieces.square.impl.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.instanceOf;
+
+public class SimplePieceFactoryTest {
+    private SimplePieceFactory factory;
+
+    @Before
+    public void setUp() throws Exception {
+        factory = new SimplePieceFactory(new SquareChessBoard(new BoardSize(2, 2)));
+    }
+
+    @Test
+    public void testCreatePiece() throws Exception {
+        Assert.assertThat(factory.createPiece(PieceType.KING), instanceOf(King.class));
+        Assert.assertThat(factory.createPiece(PieceType.QUEEN), instanceOf(Queen.class));
+        Assert.assertThat(factory.createPiece(PieceType.BISHOP), instanceOf(Bishop.class));
+        Assert.assertThat(factory.createPiece(PieceType.ROOK), instanceOf(Rook.class));
+        Assert.assertThat(factory.createPiece(PieceType.KNIGHT), instanceOf(Knight.class));
+    }
+}
